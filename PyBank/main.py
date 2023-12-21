9de7e6f3912d20e1512bf4e8574534af6f57dd87
downleft <downleft@jodys-air.attlocal.net>
2023-12-21 09:47:17 -0500

    Recording progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,6 +9,7 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 monthcount = 0 #Use this variable to count the number of months
 nettotal = 0 #Use this variable to count the net total amount of Profit/Loss
 profitchange = 0 #Use to track the change in Profit/Loss
+setmonth = 0
 maxIncrease = 0 #Use to track the greatest increase in profits
 maxDecrease = 0 #Use to track the greatest decrease in profits
 
@@ -21,8 +22,32 @@ with open(csvpath) as csvfile:
     #Read header row
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+    #row = list(csvreader)
+    #print(row[0][1])
 
     for row in csvreader:
-        monthcount = monthcount + 1
+        currentmonth = int(row[1])
+        if monthcount == 0:
+            profitchange = currentmonth
 
-    print(f"The total number of months is {monthcount} months.")
+        if currentmonth - setmonth > maxIncrease:
+            maxIncrease = currentmonth - setmonth
+        elif currentmonth - setmonth < maxDecrease:
+            maxDecrease = currentmonth - setmonth
+        #Track the number of months
+        monthcount = monthcount + 1
+        setmonth = int(row[1])
+
+        #Track the net total Profit/Losses
+        nettotal = nettotal + int(row[1])
+
+    #Track the change in Profit/Loss
+    profitchange = (profitchange - int(row[1]))/(monthcount-1)
+    profitchange = round(profitchange,2)
+
+
+    print(f"Total Months: {monthcount}")
+    print(f"Total Profit: ${nettotal}")
+    print(f"Average Change S{profitchange} per month.")
+    print(f"Greatest increases in profits {maxIncrease}")
+    print(f"Greatest decrerase in profits {maxDecrease}")
